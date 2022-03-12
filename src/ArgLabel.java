0c3a5e5a140b4c09aec96cb464b86a818ee55bb1
LachTomasz <lach.tomasz@gmail.com>
2022-03-12 23:32:01 +0100

    Initial commit

---
+++ src/ArgLabel.java
@@ -0,0 +1,13 @@
+/*
+Label example:
+
+ ******* Programowanie *******
+ *  obiektowe w języku Java  *
+ *       Name Surname        *
+ *****************************
+ */
+public class ArgLabel {
+    public static void main(String[] args) {
+
+    }
+}
