c455744d947f3c971ae400cbc93aa05dd085bf2b
LachTomasz <lach.tomasz@gmail.com>
2022-03-12 22:18:42 +0100

    Initial commit

--- src/Person.java
+++ src/Person.java
@@ -1,15 +1,25 @@
 import java.util.Locale;
-import java.lang.Character;
+/*
+Display format:
+Surname: XXXXX
+Name: YYYYY
+Surname and Name: XXXXX YYYYY
+Initials: XY
+Login: XXyyy
 
+ */
 public class Person {
     public static void main(String[] args) {
         String name = args[0];
         String surname = args[1];
-        System.out.println("Nazwisko: " + Character.toUpperCase(surname.charAt(0))+surname.substring(1));
-        System.out.println("Imię: " + Character.toUpperCase(name.charAt(0))+name.substring(1));
-        System.out.println("Nazwisko i imię: " + surname.toUpperCase() + " " + Character.toUpperCase(name.charAt(0))+name.substring(1));
+        System.out.println("Nazwisko: "
+                + surname.toUpperCase().substring(0,1)+surname.toLowerCase().substring(1,surname.length()));
+        System.out.println("Imię: "
+                + name.toUpperCase().substring(0,1)+name.toLowerCase().substring(1,name.length()));
+        System.out.println("Nazwisko i imię: " + surname.toUpperCase() + " "
+                + name.toUpperCase().substring(0,1)+name.toLowerCase().substring(1, name.length()));
         System.out.println("Iniciały: " + Character.toUpperCase(name.charAt(0))
                 + Character.toUpperCase(surname.charAt(0)));
-        System.out.println("Login: " + surname.toUpperCase().substring(0,2)+name.substring(0,3));
+        System.out.println("Login: " + surname.toUpperCase().substring(0,2)+name.toLowerCase().substring(0,3));
     }
 }
