d9efd4cb928e6a5d54f637916572432e9b6b40aa
LachTomasz <lach.tomasz@gmail.com>
2022-03-13 22:58:37 +0100

    Initial commit

--- src/ArgLabel.java
+++ src/ArgLabel.java
@@ -17,7 +17,25 @@ public class ArgLabel {
        String Surname = args[1];
        int StringLength = Name.length() + Surname.length() - 30;
 
-       //String > 0
+       //StringLength < 0
+        if (StringLength < 0){
+            if (StringLength % 2 == 0){
+                for(int i = 0; i < StringLength/2; i++){
+                    LabelLine3.s
+                }
+
+            }
+            else {
+
+            }
+        }
+
+        LabelLine3 = "* "+ Name + " " + Surname + " *";
+
+        System.out.println(LabelLine1);
+        System.out.println(LabelLine2);
+        System.out.println(LabelLine3);
+        System.out.println(LabelLine4);
 
 
     }
