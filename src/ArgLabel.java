74b18611a46b75843ad596eff78da03e0efcc266
LachTomasz <lach.tomasz@gmail.com>
2022-03-13 22:40:36 +0100

    Initial commit

--- src/ArgLabel.java
+++ src/ArgLabel.java
@@ -9,5 +9,16 @@ Label example:
 public class ArgLabel {
     public static void main(String[] args) {
 
+       String LabelLine1 = "********** Programowanie **********"; //35
+       String LabelLine2 = "*    obiektowe w języku Java      *";
+       String LabelLine3 = "";
+       String LabelLine4 = "***********************************";
+       String Name = args[0];
+       String Surname = args[1];
+       int StringLength = Name.length() + Surname.length() - 30;
+
+       //String > 0
+
+
     }
 }
